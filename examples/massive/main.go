87ee8c12ae6c6c5b41d57aa947e1221d59b1351c
Paulo Quintans Pereira <paulo.quintans@gmail.com>
2017-06-14 18:57:14 +0100

    example for massive calls of requestAll

---
+++ examples/massive/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/quintans/gomsg"
+	"github.com/quintans/toolkit/log"
+)
+
+func init() {
+	log.Register("/", log.WARN).ShowCaller(true)
+}
+
+const TOPIC = "ToPic"
+
+func wait() {
+	time.Sleep(time.Millisecond * 10)
+}
+
+func createClient(reply string) *gomsg.Client {
+	var cli = gomsg.NewClient()
+	cli.Handle(TOPIC, func() string {
+		return reply
+	})
+	cli.Connect("localhost:7777")
+	wait()
+	return cli
+}
+
+func main() {
+	server := gomsg.NewServer()
+	server.SetTimeout(time.Second)
+	server.Listen(":7777")
+
+	createClient("One")
+	createClient("Two")
+	createClient("Three")
+	createClient("Four")
+
+	wait()
+
+	for i := 0; i < 1000; i++ {
+		server.RequestAll(TOPIC, "X", func(s string) {
+			var str = s
+			if s == "" {
+				str = "[END]"
+			}
+			fmt.Println("=====>", str)
+		}, time.Second)
+
+		wait()
+	}
+}
